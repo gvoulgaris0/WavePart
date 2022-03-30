4b0127028d21a6f624bb3138707a337efdea0008
George Voulgaris <48567321+gvoulgaris0@users.noreply.github.com>
2022-03-30 16:22:25 -0400

    Update Read_WavePart_Output.m
    Just correction of typo errors in the comments explaining the code. The code was not changed.

--- Read_WavePart_Output.m
+++ Read_WavePart_Output.m
@@ -3,42 +3,42 @@
 % This is an example code to read the output parameters from the wavepart code.
 % The various parameters for each partition and the bulk parameter are output
 % as cells. For example the peak and mean wave frequencies for each partition
-% as well as for the wahole spectrum are output in the f cell. The same applies
-% for wave heights and wave directions. Below is a script that loads the data into 
-% abd creates time-series. Also an example on how to plot teh frequencies as a 
-% time-series is provided. The user shoudl adjust this script to fit her/his requirements
+% as well as for the whole spectrum are output in the f cell. The same applies
+% for wave heights and directions. Below is a script that loads the data in 
+% and creates time-series. An example on how to plot the frequencies as a 
+% time-series is also provided. The user shoulf adjust this script to fit her/his requirements.
 %
 %
-%% Load the datafile if data not already in the workspace
+%% Load the datafile. if data not already in the workspace
 %
 % load ..\data\waveparamsoutput.mat  % replace with your datafile that stores the results of wavepartition
 %  
 %% Create time series from each data containing cell
 %
-for i = 1:length(f)     % Do loop for each time-step
+for i = 1:length(f)     % For-loop for each time-step
     %
-    % Mean (fm) and Peak (fp) frequency of each partition (Hz)
+    % Mean (fm) and Peak (fp) frequency of each partition (in Hz)
     %
     ff          = f{i};        % Extract cell content for this time-step i
-    [m,n]       = size(ff);    % n-1 is the no of partitions
-    fm(i,1:n-1) = ff(1,1:n-1); % take all fmean values for all partitions
-    fp(i,1:n-1) = ff(2,1:n-1); % take all fpeak values for all partitions
+    [m,n]       = size(ff);    % n-1 is the number of partitions
+    fm(i,1:n-1) = ff(1,1:n-1); % take all mean frequency values for all partitions
+    fp(i,1:n-1) = ff(2,1:n-1); % take all peak frequency values for all partitions
     %
-    % Mean (Dm) and Peak (Dp) direction (degs) and Directional spead (sigma) for each partition.
+    % Mean (Dm) and Peak (Dp) direction (in degs) and Directional spead (sigma) for each partition.
     %
     dd          = D{i};
     Dm(i,1:n-1) = dd(1,1:n-1);
     Dp(i,1:n-1) = dd(2,1:n-1);
     si(i,1:n-1) = dd(3,1:n-1);
     %
-    % RMS (Hrms), significant (Hsig) wave height (m) and significant slope (Hsig/L)
+    % RMS (Hrms), significant (Hsig) wave height (in m) and significant slope (Hsig/L)
     %
     hh            = H{i};
     Hrms(i,1:n-1) = hh(1,1:n-1);
     Hsig(i,1:n-1) = hh(2,1:n-1);
     psi(i,1:n-1)  = hh(3,1:n-1);
     %
-    % Bulk parameters (from full (no-partitioned) specta)
+    % Bulk parameters (from the full, no-partitioned spectrum)
     %
     fbm(i)   = ff(1,n); 
     fbp(i)   = ff(2,n); 
@@ -63,19 +63,20 @@ psi(psi==0)=NaN;
 %% Example of plotting frequencies
 %
 figure
-plot(fm(:,1:2)) % Plot values for partitions 1 (swell) and 2 (wind wave)
+plot(fm(:,1:2))    % Plot values for partitions 1 (swell) and 2 (wind wave)
 hold on
 plot(fp(:,1:2))
 legend('fm - #1','fm - #2','fp - #1', 'fp - #2')
 title('Mean and Peak frequency for Partitions #1 and #2')
 ylabel('Frequency (Hz)')
 
-%% Example of plotting directions
+%% Example of plotting directions (direction convention the same as used in inputing data)
 %
 figure
-plot(Dm(:,1:2)) % Plot values for partitions 1 (swell) and 2 (wind wave)
+plot(Dm(:,1:2))   % Plot values for partitions 1 (swell) and 2 (wind wave)
 hold on
 plot(Dp(:,1:2))
 legend('Dm - #1','Dm - #2','Dp - #1', 'Dp - #2')
 title('Mean and Peak Directions for Partitions #1 and #2')
 ylabel('Direction (^o)')
+%
